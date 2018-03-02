93f9be5bf197dcb4a618d09bcd6063e0a346b9e0
TS <nymrtsoy@gmail.com>
2018-03-02 10:40:40 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+<body>
+<!-- Menu App Interface
+<!-- Based on a Dribbble shot
+<!-- that caught my eye 
+<!-- http://drbl.in/pWyx
+<!-- ---------------------------------- -->
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="script.js"></script>
+	<link href="styles.css" rel="stylesheet" media="all" />
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,15 @@
+﻿var counterModule = (function () {
+  var counter = 0;
+
+  var increaseCounter = function () {
+      counter++;
+  }
+
+  var getCounter = function () {
+    return counter;
+  }
+  return {
+	  increaseCounter : increaseCounter,
+	  getCounter : getCounter
+  }
+})();dsfsd
